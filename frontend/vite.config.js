ad4c54a7375e03d50be7f7ab1264fd1735352993
mutugi001 <collinsmut04@gmail.com>
2026-07-20 03:01:41 +0300

    Backend_URL config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       "/api": {
-        target: process.env.VITE_API_URL || "http://localhost:4000/api" || "https://ai-travel-assistant-travellis-backend.onrender.com/api",
+        target: process.env.VITE_BACKEND_API_URL || "http://localhost:4000" || "https://ai-travel-assistant-travellis-backend.onrender.com",
         changeOrigin: true,
       },
     },

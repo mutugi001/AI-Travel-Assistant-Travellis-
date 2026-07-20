55c1893b1f60cf8086339f59fcfe302e397d2734
mutugi001 <collinsmut04@gmail.com>
2026-07-20 03:22:16 +0300

    Backend_URL config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,16 +1,19 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
 
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 5173,
-    proxy: {
-      "/api": {
-        target: "https://ai-travel-assistant-travellis-backend.onrender.com",
-        changeOrigin: true,
+  return {
+    plugins: [react()],
+    server: {
+      port: 5173,
+      proxy: {
+        "/api": {
+          target: env.VITE_API_URL,
+          changeOrigin: true,
+        },
       },
     },
-  },
+  };
 });
